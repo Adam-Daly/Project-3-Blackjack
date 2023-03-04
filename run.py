bcae946e548205f5054e3a1b5d2a423432c67d38
Adam-Daly <adamacus+github@gmail.com>
2023-03-04 09:30:43 +0000

    card class init function, ranks and suits with value conversion

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        # Initializes the card with its rank and suit
+        # and assigns it a value based on its rank
+        self.rank = rank
+        self.suit = suit
+        if rank in ['10', 'Jack', 'Queen', 'King']:
+            self.value = 10
+        elif rank == 'Ace':
+            self.value = 11
+        else:
+            self.value = int(rank)
+
+           
+class Deck:
+
+
+class Player:        
+
+
+class Dealer:
+
+
+class Game:
+
+
+# main loop
